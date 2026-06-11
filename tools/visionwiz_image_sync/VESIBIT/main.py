448a839941b69841b65873dc9e4c01079a8e27d3
BitaMatt <wizai2023@163.com>
2026-06-11 10:42:21 +0800

    删除无效代码，新增上传图传同步程序代码

---
+++ tools/visionwiz_image_sync/VESIBIT/main.py
@@ -0,0 +1,17 @@
+import board
+import lcd
+import usys
+import sensor
+from visionwiz_image_sync import VisionWizImageSync
+
+
+lcd.init(freq=15000000,color=0)
+sensor.reset()
+sensor.set_pixformat(sensor.RGB565)
+sensor.set_framesize(sensor.QVGA)
+sensor.run(1)
+sensor.skip_frames(10)
+sync = VisionWizImageSync(width=320, height=240, quality=35, fps=2, show_lcd=True)
+sync.start_preview(320, 240, 35, 2, True)
+while True:
+    sync.tick()
